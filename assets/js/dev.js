34ca3f70b89b6de7c59acbc3b639fa493c58115a
Denis Manfroi <denis@denismanfroi.com>
2017-07-23 09:44:55 -0300

    Header and Footer main content

--- assets/js/dev.js
+++ assets/js/dev.js
@@ -1,1 +1,21 @@
-// alert("Olá mundo!");
+// ##
+// ** Construction functions
+// ##
+// Calc Width Element
+function calcWidthEl (elW) {return document.querySelector(elW).clientWidth || document.querySelector(elW).offsetWidth;}
+// Calc Height Element
+function calcHeightEl (elH) {return document.querySelector(elH).clientHeight || document.querySelector(elH).offsetHeight;}
+
+// Function Open Close Left Bar
+(function ocBar (){
+	var navLeftBar, btHamburguer;
+
+	navLeftBar 		= document.querySelector("#aside-navegation");
+	btHamburguer 	= document.querySelector(".menu-hamburguer");
+
+	if(document.body.clientWidth <= 414 || document.body.offsetWidth <= 414){
+		navLeftBar.classList.add("oc-bar");
+	}
+
+	btHamburguer.onclick = function () {navLeftBar.classList.toggle("oc-bar");}
+}());
