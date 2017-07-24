49fa9a09b4bd0c9ff7ff234585adce1a980fc2c1
Denis Manfroi <denis@denismanfroi.com>
2017-07-24 11:50:04 -0300

    Continuação header

--- assets/js/dev.js
+++ assets/js/dev.js
@@ -8,13 +8,17 @@ function calcHeightEl (elH) {return document.querySelector(elH).clientHeight || 
 // Function Open Close Left Bar
 (function ocBar (){
 	var navLeftBar, btHamburguer;
-
 	navLeftBar 		= document.querySelector("#aside-navegation");
 	btHamburguer 	= document.querySelector(".menu-hamburguer");
-
-	if(document.body.clientWidth <= 414 || document.body.offsetWidth <= 414){
-		navLeftBar.classList.add("oc-bar");
-	}
-
+	if(document.body.clientWidth <= 414 || document.body.offsetWidth <= 414){navLeftBar.classList.add("oc-bar");}
 	btHamburguer.onclick = function () {navLeftBar.classList.toggle("oc-bar");}
+}());
+// Confirm Close Dashboard
+(function confirmClose () {
+	var btClose;
+	btClose = document.querySelector("#close-confirm");
+	btClose.onclick = function () {
+		var msgConfirm =  confirm("Deseja realmente sair?");
+		if(msgConfirm == true){console.log("ELe quis sair mesmo.");} else{console.log("Ele decidiu permanecer.");}
+	}
 }());
