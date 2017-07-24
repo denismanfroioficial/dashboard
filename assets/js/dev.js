eebd7e46193deb4f050e458e680f8b90b15011ff
Denis Manfroi <denis@denismanfroi.com>
2017-07-24 16:45:48 -0300

    Tela de login

--- assets/js/dev.js
+++ assets/js/dev.js
@@ -11,14 +11,18 @@ function calcHeightEl (elH) {return document.querySelector(elH).clientHeight || 
 	navLeftBar 		= document.querySelector("#aside-navegation");
 	btHamburguer 	= document.querySelector(".menu-hamburguer");
 	if(document.body.clientWidth <= 414 || document.body.offsetWidth <= 414){navLeftBar.classList.add("oc-bar");}
-	btHamburguer.onclick = function () {navLeftBar.classList.toggle("oc-bar");}
+	if(btHamburguer != null){	
+		btHamburguer.onclick = function () {navLeftBar.classList.toggle("oc-bar");}
+	}
 }());
 // Confirm Close Dashboard
 (function confirmClose () {
 	var btClose;
 	btClose = document.querySelector("#close-confirm");
-	btClose.onclick = function () {
-		var msgConfirm =  confirm("Deseja realmente sair?");
-		if(msgConfirm == true){console.log("ELe quis sair mesmo.");} else{console.log("Ele decidiu permanecer.");}
+	if(btClose != null){
+		btClose.onclick = function () {
+			var msgConfirm =  confirm("Deseja realmente sair?");
+			if(msgConfirm == true){window.location = "login.html";}
+		}
 	}
 }());
